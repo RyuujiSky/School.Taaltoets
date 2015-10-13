669e94bef664caaf0b2a76fe3b8aebb1d938aa04
RyuujiSky <ryuuji-sky@hotmail.com>
2015-10-13 09:53:37 +0200

    randomized the test words.
    made it so that the first ten words (which are the same as the images)
    are set into its own array and that it shuffles said array.

--- taalToets/ToetsModel.java
+++ taalToets/ToetsModel.java
@@ -8,15 +8,22 @@ public class ToetsModel
    private Vraag[] vragen = {new Vraag("Boom","Boom.png"),new Vraag("Aap","Aap.png"),new Vraag("Hond","Hond.png"),new Vraag("Kat","Kat.png"), new Vraag("Maan","Maan.png"),new Vraag("Huis","Huis.png"),
        new Vraag("Roos","Roos.png"),new Vraag("Vis","Vis.png"),new Vraag("Sok","Sok.png"),new Vraag("Pen","Pen.png")};
       
+    private Vraag[] woorden = new Vraag[10];
+    
     public ToetsModel(ToetsWorld w)
     {
         this.w = w;
+        
+        for(int i=0; i<10; i++)
+        {
+            woorden[i]=vragen[i];
+        }
     }
     
     public String getVragen(int i)
     {
        String v;
-       v=vragen[i].getVraag();
+       v=woorden[i].getVraag();
        return v;
     }
     
@@ -41,6 +48,7 @@ public class ToetsModel
     public void shuffleArray()
     {
         java.util.Collections.shuffle(java.util.Arrays.asList(vragen));
+        java.util.Collections.shuffle(java.util.Arrays.asList(woorden));
     }
 
 }
